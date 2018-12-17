ba39ed0dd6a130c137c26641eb58214620dbbab7
Niklas Moritz <niklas.moritz@gmail.com>
2018-12-17 16:28:23 +0100

    Day 15

--- Advent2018_15.cs
+++ Advent2018_15.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class Advent2018_15
@@ -16,40 +17,73 @@ public class Advent2018_15
 #G..G..G#
 #########";
         var inputData = input.Split(new string[] { "\r\n" }, StringSplitOptions.None).Select(x => x.ToCharArray());
-        var map = new Cell[inputData.First().Count(),inputData.Count()];
-        for(var x = 0; x < inputData.First().Count() - 1; x++)
+        var map = new Cell[inputData.First().Count(), inputData.Count()];
+
+        var walls = new List<Cell>();
+        var elfs = new List<Cell>();
+        var goblins = new List<Cell>();
+
+        for (var x = 0; x < inputData.First().Count() - 1; x++)
         {
-            for(var y = 0; y < inputData.Count() - 1; y++)
+            for (var y = 0; y < inputData.Count() - 1; y++)
             {
                 var type = inputData.ElementAt(y).ElementAt(x);
-                if(type =='#')
-                    map[x, y] = new Cell { Type = CellType.Wall };
-                else if(type =='G')
-                    map[x, y] = new Cell    { Type = CellType.Goblin };
-                else if(type =='E')
-                    map[x, y] = new Cell { Type = CellType.Elf };
+                if (type == '#')
+                    walls.Add(new Cell { Type = CellType.Wall, Coord = (x, y) });
+                else if (type == 'G')
+                    goblins.Add(new Cell { Type = CellType.Goblin, Coord = (x, y) });
+                else if (type == 'E')
+                    elfs.Add(new Cell { Type = CellType.Elf, Coord = (x, y) });
             }
         }
-
-        for(var y = 0; y < map.GetUpperBound(1); y++)
+        var combatants = elfs.Concat(goblins).OrderBy(x => x.Coord.Y).ThenBy(x => x.Coord.Y);
+        foreach (var combatant in combatants.Take(1))
         {
-            for(var x = 0; x < map.GetUpperBound(0); x++)
+            var enemies = combatant.Type == CellType.Goblin ? elfs : goblins;
             {
-                var cell = map[x, y];
-                if(cell == null || cell.Type == CellType.Wall)
-                    return;
-                else 
+                foreach(var enemy in enemies.Take(1))
                 {
-                    var closest = findCloses((x, y), cell, map); 
+                    var distance = findDistance(combatant, enemies, combatants.Concat(walls).Except(new Cell[] { combatant, enemy } ));
+                    Console.WriteLine(distance);
                 }
             }
         }
-        
+
     }
 
-    private (int x, int y) findCloses((int x, int y) p, Cell cell, Cell[,] map)
+    private int findDistance(Cell combatant, IEnumerable<Cell> enemies, IEnumerable<Cell> obsticles)
     {
-        throw new NotImplementedException();
+        var visited = new List<(int X, int Y)>();
+        Queue<Node> q = new Queue<Node>();
+        q.Enqueue(new Node() { Coord = combatant.Coord, Distance = 0 });
+        while (q.Count > 0)
+        {
+            Node current = q.Dequeue();
+            if (current == null)
+                continue;
+            var enemy = enemies.FirstOrDefault(x => x.Coord.X == current.Coord.X && x.Coord.Y == current.Coord.Y);
+            if(enemy != null)
+                return current.Distance;
+
+            var left = obsticles.FirstOrDefault(x => x.Coord.X == current.Coord.X - 1 && x.Coord.Y == current.Coord.Y);
+            var right = obsticles.FirstOrDefault(x => x.Coord.X == current.Coord.X + 1 && x.Coord.Y == current.Coord.Y);
+            var top = obsticles.FirstOrDefault(x => x.Coord.X == current.Coord.X && x.Coord.Y == current.Coord.Y - 1);
+            var bottom = obsticles.FirstOrDefault(x => x.Coord.X == current.Coord.X && x.Coord.Y == current.Coord.Y + 1);
+            var neighbours = new (int X, int Y)[] 
+            { 
+                left != null ? (left.Type == CellType.Wall ? (-1, -1) : left.Coord) : (current.Coord.X - 1, current.Coord.Y), 
+                right != null ? (right.Type == CellType.Wall ? (-1, -1) : right.Coord): (current.Coord.X + 1, current.Coord.Y), 
+                top != null ? (top.Type == CellType.Wall ? (-1, -1) : top.Coord) : (current.Coord.X, current.Coord.Y - 1), 
+                bottom != null ? (bottom.Type == CellType.Wall ? (-1, -1) : bottom.Coord) : (current.Coord.X, current.Coord.Y + 1) 
+            };
+
+            foreach(var n in neighbours.Where(x => x.X != -1 ).Except(visited))
+            {
+                q.Enqueue(new Node { Coord = n, Distance = current.Distance + 1});
+                visited.Add(n);
+            }
+        }
+        return 0;
     }
 
     private class Cell
@@ -58,7 +92,17 @@ public class Advent2018_15
 
         public int Health { get; set; }
 
+        public (int X, int Y) Coord { get; set; }
+
     }
+
+    private class Node
+    {
+        public (int X, int Y) Coord { get; set; }
+        public int Distance { get; set; }
+
+    }
+
     public enum CellType
     {
         Goblin,
